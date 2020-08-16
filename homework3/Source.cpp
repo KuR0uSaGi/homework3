e5b6d72ba2c816ac80ee5458e6c493124d176567
tee worawitch <teenoy92545@gmail.com>
2020-08-16 20:53:26 +0700

    ...

--- homework3/Source.cpp
+++ homework3/Source.cpp
@@ -1,18 +1,21 @@
-#include<stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
-int main()
-{
-	int a,b,c,d;
-	scanf_s("%d %d %d %d", &a, &b, &c, &d);
-	a = a + b;
-	b = a - b;
-	a = a - b;
-	b = b + d;
-	d = b - d;
-	b = b - d;
-	d = c + d;
-	c = d - c;
-	d = d - c;
-	printf("%d %d %d %d", a, b, c, d);
-
+#include<stdio.h> 
+#include<math.h>
+int main() {
+    long int a, suma, sumb, b;
+    scanf_s("%ld", &a);
+    b = floor(a);
+    suma = a % 2;
+    sumb = a % 3;
+    if (a > 30 || a == 0 || a < 0 || a != b)
+        printf("Invalid date.");
+    else if (a <= 30 && a != 0 && suma == 0 && sumb == 0)
+        printf("Both of them.");
+    else if (a <= 30 && a != 0 && suma != 0 && sumb != 0)
+        printf("Nothing.");
+    if (a <= 30 && a != 0 && suma == 0 && sumb != 0)
+        printf("TONMAI A");
+    else if (a <= 30 && a != 0 && sumb == 0 && suma != 0)
+        printf("TONMAI B");
+    return 0;
 }
